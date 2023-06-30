0a723d80847405f1557f464c52c76bdf431bc85d
Nabil Wassim <nabilwssm@gmail.com>
2023-06-30 05:43:17 -0700

    زززز

--- public/src/components/Contacts.jsx
+++ public/src/components/Contacts.jsx
@@ -168,107 +168,6 @@ export default function Contacts({ contacts, changeChat }) {
 // `;
 
 
-// const Container = styled.div`
-//   display: grid;
-//   grid-template-rows: 10% 75% 15%;
-//   overflow: hidden;
-//   background-color: #040C20;
-
-//   .brand {
-//     display: flex;
-//     align-items: center;
-//     gap: 1rem;
-//     justify-content: center;
-
-//     img {
-//       height: 2rem;
-//     }
-
-//     h3 {
-//       color: white;
-//       text-transform: uppercase;
-//     }
-//   }
-
-//   .contacts {
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     overflow: auto;
-//     gap: 0.8rem;
-
-//     &::-webkit-scrollbar {
-//       width: 0.2rem;
-
-//       &-thumb {
-//         background-color: #ffffff39;
-//         width: 0.1rem;
-//         border-radius: 1rem;
-//       }
-//     }
-
-//     .contact {
-//       background-color: #0D1C30;
-//       min-height: 5rem;
-//       cursor: pointer;
-//       width: 90%;
-//       border-radius: 0.2rem;
-//       padding: 0.4rem;
-//       display: flex;
-//       gap: 1rem;
-//       align-items: center;
-//       transition: 0.5s ease-in-out;
-
-//       .avatar {
-//         img {
-//           height: 3rem;
-//         }
-//       }
-
-//       .username {
-//         h3 {
-//           color: white;
-//         }
-//       }
-//     }
-
-//     .selected {
-//       background-color: #187bcd;
-//     }
-//   }
-
-//   .current-user {
-//     background-color: #0D1A30;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     gap: 2rem;
-
-//     .avatar {
-//       img {
-//         height: 4rem;
-//         max-inline-size: 100%;
-//       }
-//     }
-
-//     .username {
-//       h2 {
-//         color: white;
-//       }
-//     }
-
-//     @media screen and (min-width: 720px) and (max-width: 1080px) {
-//       gap: 0.5rem;
-
-//       .username {
-//         h2 {
-//           font-size: 1rem;
-//         }
-//       }
-//     }
-//   }
-// `;
-
 const Container = styled.div`
   display: grid;
   grid-template-rows: 10% 75% 15%;
@@ -359,11 +258,17 @@ const Container = styled.div`
     }
 
     @media screen and (min-width: 720px) and (max-width: 1080px) {
-      gap: 0.5rem;
+      gap: 0.2rem;
       
+      .avatar {
+        img {
+          height: 2rem;
+          max-inline-size: 100%;
+        }
+      }
       .username {
         h2 {
-          font-size: 1rem;
+          font-size: 0.5rem;
         }
       }
     }
