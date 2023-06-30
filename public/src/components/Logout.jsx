a50ddf947adbadad56dd96ee4e4d9c048fd2dfd3
Nabil Wassim <nabilwssm@gmail.com>
2023-06-30 05:14:50 -0700

    responsive2

--- public/src/components/Logout.jsx
+++ public/src/components/Logout.jsx
@@ -23,21 +23,43 @@ export default function Logout() {
   );
 }
 
+// const Button = styled.button`
+//   // display: flex;
+//   // justify-content: center;
+//   // align-items: center;
+//   // padding: 0.5rem;
+//   // border-radius: 0.5rem;
+//   // // background-color: #9a86f3;
+//   // background-color: #187bcd;
+//   // border: none;
+//   // cursor: pointer;
+//   // svg {
+//   //   font-size: 1.3rem;
+//   //   // color: #ebe7ff;
+//   //   color: #E7F2FF;
+//   // }
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 0.5rem;
+//   border-radius: 0.5rem;
+//   background-color: #187bcd;
+//   border: none;
+//   cursor: pointer;
+//   svg {
+//     font-size: 1.3rem;
+//     color: #E7F2FF;
+//   }
+//   @media screen and (max-width: 768px) {
+//     padding: 0.3rem;
+//     svg {
+//       font-size: 1rem;
+//     }
+//   }
+// `;
+
+
 const Button = styled.button`
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
-  // padding: 0.5rem;
-  // border-radius: 0.5rem;
-  // // background-color: #9a86f3;
-  // background-color: #187bcd;
-  // border: none;
-  // cursor: pointer;
-  // svg {
-  //   font-size: 1.3rem;
-  //   // color: #ebe7ff;
-  //   color: #E7F2FF;
-  // }
   display: flex;
   justify-content: center;
   align-items: center;
